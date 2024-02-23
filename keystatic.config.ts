222bac3050fb59f60a7b1efd4a532818fbdef4d6
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-02-23 10:25:03 +0700

    feat: :sparkles: initialize tailwind and keystatic

---
+++ keystatic.config.ts
@@ -0,0 +1,27 @@
+import { config, fields, collection } from "@keystatic/core";
+
+export default config({
+  storage: {
+    kind: "local",
+  },
+
+  collections: {
+    posts: collection({
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        heroImage: fields.image({ label: "Hero Image" }),
+        content: fields.document({
+          label: "Content",
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: true,
+        }),
+      },
+    }),
+  },
+});
