df41fda10d13d2b2feb1626ac0d34f07a2c4bf08
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-02-28 09:01:48 +0700

    feat(package.json): :heavy_plus_sign: install cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), markdoc()],
   output: "hybrid",
+  adapter: cloudflare()
 });
