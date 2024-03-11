6febfe7a5f5675e96da791321770550e3e8390af
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-03-11 17:37:10 +0700

    ci(tailwind.config): :wrench: make the base color to be black & white

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,19 +7,6 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
-      primary: {
-        50: "#eefffc",
-        100: "#c5fffa",
-        200: "#8bfff5",
-        300: "#4afef0",
-        400: "#15ece2",
-        500: "#00d0c9",
-        600: "#00a8a5",
-        700: "#007b7b",
-        800: "#066769",
-        900: "#0a5757",
-        950: "#003235",
-      },
       black: colors.black,
       white: colors.white,
       ...colors,
