2567d11b57f0d794dbe2b1a0cfd30b3813caecc5
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-03-13 11:31:21 +0700

    fix(astro.config): :bug: adjust site

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://v4.yehezgun.com/",
+  site: import.meta.env.PUBLIC_SITE,
   integrations: [
     tailwind(),
     react(),
